3b8d307e060d7dd05bf2db68d2c2f68e87f8a18b
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-06-15 21:47:18 +0530

    An image added on home page.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => res.render('index'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
