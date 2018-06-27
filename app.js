5c4319117604de36721fbc57e2fd538d65d3e42c
Emma Paris <gaurisutar88@gmail.com>
2018-06-27 17:52:24 +0530

    menu bar code

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'html');
 
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) => res.render('index'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
