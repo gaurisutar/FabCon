84bcfd76d287463f9e70bbbf5726eede26def50c
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-11-04 22:27:27 +0530

    moved current project to archive folder. Starting a fresh.

--- archive/app.js
+++ archive/app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('ShreeFabCon app listening on port 3000!'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
