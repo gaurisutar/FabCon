8aa85c672d50ab85691caae26894c5570df7856d
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-11-08 22:09:43 +0530

    Linking all the pages with Navbar.

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.set('view engine', 'html');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/about-us', (req, res) => res.render('about-us'));
+app.get('/gallery', (req, res) => res.render('gallery'));
+app.get('/team', (req, res) => res.render('team'));
+app.get('/contact-us', (req, res) => res.render('contact-us'));
 
 app.listen(3000, () => console.log('ShreeFabCon app listening on port 3000!'));
